61e581fcc1c9b8ee85d6626bb5be9705b9cd1d7d
erikabelanich <161361629+erikabelanich@users.noreply.github.com>
2024-06-04 12:06:35 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+    // Show loading symbol on link click
+    $(".navbar-nav a").click(function () {
+        var href = $(this).attr("href");
+        $("#loading").fadeIn(); // Show loading symbol
+
+        setTimeout(function () {
+            window.location.href = href; // Redirect after a delay (simulated loading time)
+        }, 2000); // Simulated loading time (2000 milliseconds = 2 seconds)
+        
+        return false; // Prevent default link behavior
+    });
+});
+
