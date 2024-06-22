b6855ed26d97cdaf644ae157117b96e7aced1ae8
erikabelanich <161361629+erikabelanich@users.noreply.github.com>
2024-06-21 22:06:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,27 +12,3 @@ $(document).ready(function () {
     });
 });
 
-// Change cursor to camera icon when not hovering over links or buttons
-    document.addEventListener('DOMContentLoaded', function() {
-      document.body.addEventListener('mouseover', function(event) {
-        if (event.target.tagName !== 'A' && event.target.tagName !== 'BUTTON') {
-          document.body.style.cursor = 'url("Mouse.png"), auto';
-        }
-      });
-
-      document.body.addEventListener('mouseout', function(event) {
-        if (event.target.tagName !== 'A' && event.target.tagName !== 'BUTTON') {
-          document.body.style.cursor = 'url("Mouse.png"), auto';
-        }
-      });
-
-      // Reset cursor to default when hovering over links or buttons
-      document.querySelectorAll('a, button').forEach(function(element) {
-        element.addEventListener('mouseover', function() {
-          document.body.style.cursor = 'pointer';
-        });
-        element.addEventListener('mouseout', function() {
-          document.body.style.cursor = 'url("Mouse.png"), auto';
-        });
-      });
-    });
