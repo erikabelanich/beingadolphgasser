54b60428533bed052210a07bdfbfed4e24b6f0e9
erikabelanich <161361629+erikabelanich@users.noreply.github.com>
2024-07-08 09:11:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
+
+// popup window
+
+document.getElementById('openPopup').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'block';
+});
+
+document.getElementsByClassName('close')[0].addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target == document.getElementById('popup')) {
+        document.getElementById('popup').style.display = 'none';
+    }
+});
